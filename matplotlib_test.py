9a183fade8341d7d066925976c338725dfb5f2bc
duo0309 <51026293+duo0309@users.noreply.github.com>
2020-05-21 15:32:29 +0900

    Update matplotlib_test.py

--- matplotlib_test.py
+++ matplotlib_test.py
@@ -2,8 +2,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('classic')
 
-x = np.arange(0,500+1,1)
+
+# ------------------------------------------
+# 0 dimension plot
+# ------------------------------------------
+r = np.random.randn(100)
+
+plt.plot(r, 'o')
+plt.show()
+
+
+# ------------------------------------------
+# 1 dimension plot
+# ------------------------------------------
+x = np.arange(0, 100, 2)
 y = np.log(x)
 
-[plt.plot(x,y+i,'o') for i in range(10)]
+[plt.plot(x, y+i, 'o') for i in range(10)]
+plt.show()
+
+# ------------------------------------------
+# 1 dimension plot
+# ------------------------------------------
+
+
+def f(x, y):
+    return np.sin(x) ** 10 + np.cos(10 + y * x) * np.cos(x)
+
+
+x = np.linspace(0, 5, 50)
+y = np.linspace(0, 5, 40)
+
+X, Y = np.meshgrid(x, y)
+Z = f(X,Y)
+
+plt.contourf(X, Y, Z, 20, cmap='RdGy')
+plt.colorbar()
 plt.show()
